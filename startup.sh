1977466734c0bd3f8827a3aa17448242887255c1
Joseph Luck <joseph.luck@sky.com>
2016-12-04 17:52:38 +0000

    Adding startup.sh to be run on startup of the PI as a cron

---
+++ startup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+echo "Starting gonglord" > /tmp/gonlord_log.txt
+
+# Print the IP address
+_IP=$(hostname -I) || true
+if [ "$_IP" ]; then
+  printf "My IP address is %s\n" "$_IP"
+fi
+
+# Kill PIGPIO daemon
+kill pigpiod &
+
+# Run PIGPIO daemon
+/usr/local/bin/pigpiod &
+
+# Run the webhook listener
+python /home/pi/gonglord/server.py &
+
+# Run the wifi connection monitor
+sudo /home/pi/gonglord/network-monitor.sh &
