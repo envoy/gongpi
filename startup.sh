c4c19285f3ffa4bec8ea1e5b5cbcf109ca8af670
Joseph Luck <joseph.luck@sky.com>
2016-12-06 21:08:06 +0000

    Updating startup.sh

--- startup.sh
+++ startup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 
 # Ensure that the script has run on boot
-sudo echo "Starting gonglord on $(date)" > /tmp/gonglord_log.txt
+echo "Starting gonglord on $(date)" > /tmp/gonglord_log.txt &
 
 # Kill PIGPIO daemon
-sudo kill pigpiod &
+kill pigpiod &
 
 # Run PIGPIO daemon
-sudo /usr/local/bin/pigpiod &
+/usr/local/bin/pigpiod &
 
 # Run the webhook listener
-sudo python /home/pi/gonglord/server.py &
+python /home/pi/gonglord/server.py &
 
 # Run the wifi connection monitor
 sudo /home/pi/gonglord/network-monitor.sh &
