4fa3108445dea88a8c4c427e18fc3074f6ce02f7
Justin Ramos <justin@gigster.com>
2017-08-03 18:03:32 -0700

    fixes

--- server.py
+++ server.py
@@ -12,24 +12,23 @@ urls = ('/.*', 'hooks')
 app = web.application(urls, globals())
 
 class hooks:
-  def logRequest(self):
-    data = web.data()
+  def logRequest(self, data):
     ip = web.ctx['ip']
     timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
     f = open('/home/pi/gongpi/server.log','a')
     f.write('[' + ip + '] ' + timestamp + ': ' + data + '\n')
     f.close()
 
   def POST(self):
     try:
-      self.logRequest()
+      data = web.data()
+      self.logRequest(data)
       server_json = json.loads(data)
     except ValueError, e:
       return '200 OK'
 
     if server_json['type'] in ['charge.succeeded']:
-      os.system("python /home/pi/gong.py 1")
+      os.system("python /home/pi/gongpi/gong.py 1")
     else:
       pass
 
